6ee77878e47c89ec65aaa7f5e6241ef949c09ab4
Yoichi Takai <takai@cmscom.jp>
2019-10-09 16:21:44 +0900

    :sparkles: Write to .po

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import csv
 
+import polib
+
+
 def main():
     with open('empty.tsv', newline='') as tsv:
         reader = csv.reader(tsv, delimiter='\t')
@@ -10,6 +13,12 @@ def main():
             comment.append(row[0])
             id.append(row[2])
             translated.append(row[5])
+
+    po = polib.POFile()
+    length = len(id)
+    for i in range(1, length):
+        po.append(polib.POEntry(msgid=id[i], msgstr=translated[i]))
+    po.save('empty.po')
 
 
 if __name__ == '__main__':
