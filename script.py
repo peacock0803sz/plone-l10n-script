8eeb9ffe25c5023af4d80f3087e73de5b9588be9
Yoichi Takai <takai@cmscom.jp>
2019-10-09 17:19:16 +0900

    :construction: Write .po without polib

--- script.py
+++ script.py
@@ -1,31 +1,22 @@
 import csv
 
-import polib
-
 POFILE = 'empty.po'
 
 def main():
     with open('empty.tsv', newline='') as tsv:
         reader = csv.reader(tsv, delimiter='\t')
         comment = []
-        id = []
-        translated = []
+        msgid = []
+        msgstr = []
         for row in reader:
             comment.append(row[0])
-            id.append(row[2])
-            translated.append(row[5])
+            msgid.append(row[2])
+            msgstr.append(row[5])
 
-    po = polib.POFile()
-    length = len(id)
-    for i in range(1, length):
-        po.append(polib.POEntry(msgid=id[i], msgstr=translated[i]))
-    po.save(POFILE)
-
-    with open(POFILE, mode='r') as f:
-        read = f.readlines()
-    for i in range(1, length, 3):
-        read.insert(i, f'#: {comment[i]}')
-
+    length = len(msgid)
+    with open(POFILE, mode='w') as f:
+        for i in range(1, length):
+            f.write(f'#: {comment[i]}\nmsgid "{msgid[i]}"\nmsgstr "{msgstr[i]}"\n\n')
 
 if __name__ == '__main__':
     main()
